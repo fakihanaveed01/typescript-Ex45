15b6c5d1c6083eceb1e35c57e001e04b54f118e9
fakihanaveed01 <fakihanaveed45@gmail.com>
2024-04-08 01:38:12 -0700

    Assignment 45

---
+++ app.js
@@ -0,0 +1,2 @@
+var personName = "Fakiha";
+console.log("Hello ".concat(personName, ", would you like to learn some python today?"));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personName = "Fakiha";
+console.log(`Hello ${personName}, would you like to learn some python today?`);
